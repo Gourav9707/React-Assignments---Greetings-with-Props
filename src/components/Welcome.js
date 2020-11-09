d9e2f3b0d9f823067e25d6235e6335fc3bc42912
Gourav9707 <67501980+Gourav9707@users.noreply.github.com>
2020-11-09 18:27:46 +0530

    change

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ import "../styles/App.css";
 export default function Welcome(props) {
   return (
     <>
-      <h1>Hey !{props.name}</h1>
+      <h1>Hey ! {props.name}</h1>
       <h2>Welcome to Newton School.</h2>
     </>
   );
